4928afc9632efe04fbc59fe8ec5c277033dff93c
Gino Messmer <8465892+ginomessmer@users.noreply.github.com>
2019-08-05 18:54:15 +0200

    Add domain project and add user aggregrate (#1)

---
+++ ProjectRedhead.Domain/UserAggregrate/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectRedhead.Domain.UserAggregrate
+{
+    /// <summary>
+    /// Stellt ein Domain Model eines Nutzers dar.
+    /// </summary>
+    public class User : Entity
+    {
+        /// <summary>
+        /// Der Nutzername des Nutzers.
+        /// </summary>
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// Die Rolle des Nutzers.
+        /// </summary>
+        public UserRole Role { get; set; }
+
+        /// <summary>
+        /// Die erworbenen Punkte des Nutzers.
+        /// </summary>
+        public int Points { get; set; }
+
+
+        public User(string displayName)
+        {
+            DisplayName = displayName;
+        }
+    }
+}
