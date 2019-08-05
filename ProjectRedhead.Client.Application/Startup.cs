cabf99e75193dea30523e1c6a0be6f9bc5eb6f4c
Gino Messmer <git@ginomessmer.me>
2019-08-05 23:57:24 +0200

    Add blank host project for SPA

--- ProjectRedhead.Client.Application/Startup.cs
+++ ProjectRedhead.Client.Application/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ProjectRedhead.Client.Host
+namespace ProjectRedhead.Client.Application
 {
     public class Startup
     {
