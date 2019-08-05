0dffbb9bf2171938b7a234b09baf0d12a9d34597
Gino Messmer <8465892+ginomessmer@users.noreply.github.com>
2019-08-06 00:19:29 +0200

    Add SPA client app (#10)
    * Discord authentication (#5)
    
    * Add Discord OAuth
    
    * Add challenge HTTP method
    
    * Add finalize auth request
    
    * Finalize auth
    
    * Adjust readme (#7)
    
    * Add SPA client app (#9)
    
    * Add blank host project for SPA
    
    * Add React app

--- ProjectRedhead.Client.Application/Pages/Error.cshtml.cs
+++ ProjectRedhead.Client.Application/Pages/Error.cshtml.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ProjectRedhead.Client.Host.Pages
+namespace ProjectRedhead.Client.Application.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
--- ProjectRedhead.Client.Application/Program.cs
+++ ProjectRedhead.Client.Application/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace ProjectRedhead.Client.Host
+namespace ProjectRedhead.Client.Application
 {
     public class Program
     {
