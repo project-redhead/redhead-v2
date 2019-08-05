c469ba95f3b97fc0dea89471ccc6096d11a6ecbb
Gino Messmer <8465892+ginomessmer@users.noreply.github.com>
2019-08-06 00:14:59 +0200

    Add SPA client app (#9)
    * Add blank host project for SPA
    
    * Add React app

--- ProjectRedhead.Client.Application/Controllers/SampleDataController.cs
+++ ProjectRedhead.Client.Application/Controllers/SampleDataController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ProjectRedhead.Client.Host.Controllers
+namespace ProjectRedhead.Client.Application.Controllers
 {
     [Route("api/[controller]")]
     public class SampleDataController : Controller
