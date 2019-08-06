2d381c4bec456c94cd0f877841a1baca673c2c30
Gino Messmer <git@ginomessmer.me>
2019-08-06 22:19:34 +0200

    Add sidebar item component

--- ProjectRedhead.Client.Application/ClientApp/src/components/sidebar/index.tsx
+++ ProjectRedhead.Client.Application/ClientApp/src/components/sidebar/index.tsx
@@ -5,17 +5,17 @@ import './sidebar.scss';
 const Sidebar = () => {
 	return (
 		<div className="sidebar-container">
-			<a href="" className="item">
-				<i className='uil uil-calendar-alt'></i>
-			</a>
-			<a href="" className="item">
-				<i className='uil uil-home-alt'></i>
-			</a>
-			<a href="" className="item">
-				<i className='uil uil-metro'></i>
-			</a>
+			<SidebarItem iconName="calendar-alt" />
+			<SidebarItem iconName="home-alt" />
+			<SidebarItem iconName="metro" />
 		</div>
 	)
 };
+
+const SidebarItem = (props: { iconName: string }) => (
+	<a className="item">
+		<i className={'uil uil-' + props.iconName}></i>
+	</a>
+);
 
 export default Sidebar;
